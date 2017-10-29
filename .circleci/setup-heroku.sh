0021c3f94ca72e74e3a09539c85f0049f7e57a09
johnSamilin <masteralex@inbox.ru>
2017-10-29 13:54:01 +0300

    no message

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,10 +4,7 @@
   sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
   sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
      
-  cat > ~/.netrc << EOF
-  machine api.heroku.com
-    login $HEROKU_LOGIN
-    password $HEROKU_API_KEY EOF
+  cat > ~/.netrc "machine api.heroku.com\n \tlogin $HEROKU_LOGIN\n \tpassword $HEROKU_API_KEY"
 
   cat >> ~/.ssh/config << EOF
   VerifyHostKeyDNS yes
