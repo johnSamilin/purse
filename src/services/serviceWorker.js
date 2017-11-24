a5d8f97d9aff2b40cc1e9f8e23f2e68f1b4292f5
johnSamilin <masteralex@inbox.ru>
2017-11-24 23:36:10 +0300

    no message

--- src/services/serviceWorker.js
+++ src/services/serviceWorker.js
@@ -1,12 +1,15 @@
-const version = 'v1.0.7';
+const version = 'v1.0.9';
 const baseUrl = 'https://purseapp.herokuapp.com';
 const precachedUrls = [
+  '/',
+  '/index.html',
   '/bootstrap.min.css',
   '/giphy-downsized.gif',
   '/loader.gif',
   '/app.js',
-  '/index.html',
-  '/',
+  '/app.css',
+  '/vendor.js',
+  '/material_icons.woff2',
 ];
 
 self.addEventListener('install', function(event) {
