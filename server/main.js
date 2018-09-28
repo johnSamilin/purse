42d9275ece7237d0ca67bf4b85dbc31eda5a80ac
ASaltykov <temp.kroogi@gmail.com>
2018-09-28 18:51:41 +0300

    gzip

--- server/main.js
+++ server/main.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const compress = require('compression');
+const http = require('http');
 
 const app = express();
 
 // Apply gzip compression
 app.use(compress());
-app.use(express.static('/'));
+app.use(express.static('.'));
 
-module.exports = app;
+http.createServer(app).listen(80);
